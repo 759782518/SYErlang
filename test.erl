37c3d8ba044913314c74a8472733de8106b1f1e1
root <root@DESKTOP-BFMUJBI.localdomain>
2022-06-16 16:21:44 +0800

    SYErlang

---
+++ test.erl
@@ -0,0 +1,17 @@
+%%%-------------------------------------------------------------------
+%%% @author Administrator
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 16. 6月 2022 14:15
+%%%-------------------------------------------------------------------
+-module(test).
+-author("Administrator").
+
+%% API
+-export([start/0]).
+
+start()->
+  io:format("hello erlang").
+
